b3965df2176c72e43b2db4e921d153008800e8eb
Nicolas.Hainak <nicolas.hainak@u-psud.fr>
2020-02-12 16:58:31 +0100

    le1202

--- laby-jupyter/laby_en_cours_de_construction.py
+++ laby-jupyter/laby_en_cours_de_construction.py
@@ -36,3 +36,54 @@ def gauche(x):
     
 def droite(x):
     return str((int(x)+1)%4)
+
+
+position = namedtuple ("position",["i","j"])
+
+def load (string):
+    tab = []
+    f = open(string)
+    for line in f:
+        if (line[0] == "o"):
+            x = line.split()
+            tab.append(x)
+    return tab
+class Labyrinth:
+    def __init__(self,s):
+        self.tile=Tiles
+        self._won =False
+        self.carry=self.tile.Void
+        self.direction="0"
+        self.board=load(s)
+        self.position=position(5,5)
+        self.message=""
+    def size(self):
+        return (len(self.board),len(self.board[0]))
+    def devant(self):
+        po= position(self.position.i+int(direction[self.direction][0]),self.position.j+int(direction[self.direction][1]));
+        return po
+    def gauche(self):
+        self.direction= str((int(self.direction)-1)%4)
+        self.message=""
+        return True;
+    def droite(self):
+        self.direction= str((int(self.direction)+1)%4)
+        self.message=""
+        return True;
+    def win(self):
+        self._won=True
+        message="J'ai gagné !"
+    def won(self):
+        return self._won
+    def pose(self):
+        if (self.carry==.t.Rock and (regarde()==t.Void or regarde()==t.Web or regarde()==t.SmallWeb or regarde()==t.SmallRock  )):
+            self.carry=t.Void
+            board.set(devant(),t.Rock)
+            message=""
+            return True
+        message="Je ne peux pas poser."
+        return False
+    def regarde(self):
+        message=""
+        return self.board.get(devant())
+    
