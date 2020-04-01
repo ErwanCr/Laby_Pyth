5dc1dd3e8779ee3ae83653046d36c268cc6e1b46
Nicolas <nico_0104@live.fr>
2020-04-01 17:58:06 +0200

    coursdu1avril

---
+++ laby-jupyter/global_fr.py
@@ -0,0 +1,32 @@
+from laby import *
+
+l = 
+
+
+def debut():
+    l.debut()
+    
+def avance():
+    l.avance()
+    
+def droite():
+    l.droite()
+    
+def gauche():
+    l.gauche()
+    
+def pose():
+    l.pose()
+    
+def prend():
+    l.prend()
+    
+def ouvre():
+    l.ouvre()
+
+def regarde():
+    l.regarde()
+    
+def win():
+    l.win()
+    
