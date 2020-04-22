01cb63dc03b0abcf5a2d2b2abebb687eca14e13a
Nicolas <nico_0104@live.fr>
2020-04-22 23:03:38 +0200

    modif today

--- laby-jupyter/timer.py
+++ laby-jupyter/timer.py
@@ -1,33 +1,56 @@
 from threading import Timer,Thread,Event
 
 
-class perpetualTimer():
+class PerpetualTimer():
 
-    def __init__(self,t,hFunction):
-        self.t=t
-        self.hFunction = hFunction
-        self.thread = Timer(self.t,self.handle_function)
+    def __init__(self,fps,tick):
+        self.fps=fps
+        self.tick = tick
+        self.thread = None #Timer(self.t,self.handle_function)
         self.isrunning=False
-
+        self.run()
+        
     def handle_function(self):
-        #controle du temps depart stop decider combien on attend ou non +s=cancel et restart //regarder si on peu chanegr la valeur dans pyton du timer 
-        self.hFunction()
-        self.thread = Timer(self.t,self.handle_function)
+        #controle du temps depart stop decider combien on attend ou non +s=cancel et restart 
+        # //regarder si on peu chanegr la valeur dans pyton du timer 
+        assert self.thread is not None
+        assert self.isrunning 
+        self.tick()
+        self.thread.cancel()
+        self.thread = Timer(1/self.fps,self.handle_function)
         self.thread.start()
 
     def set_fps(self,fps):
-        self.t=fps
+        #convertir fps en seconde 
+        self.fps=fps    
+        if self.isrunning:
+            self.cancel()
+            self.start()
+        
+    
+    def run(self):
+        if (self.isrunning):
+            return 
+        self.thread = Timer(1/self.fps,self.handle_function)
+        self.thread.start()
+        self.isrunning=True   
 
     def running(self):
         return self.isrunning
-
+    
+    #peut etre enlever
     def start(self):
+        if (self.isrunning):
+            return 
+        self.thread = Timer(1/self.fps,self.handle_function)
         self.thread.start()
         self.isrunning=True
 
     def cancel(self):
-        self.thread.cancel()
-        self.isrunning=False
+        if self.isrunning:
+            self.thread.cancel()
+            self.thread=None
+            self.isrunning=False
 
 def printer():
     print ('ipsem lorem')
